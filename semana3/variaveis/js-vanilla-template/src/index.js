da86879da36d3dffb69daa992eb46f19021c49b8
Mila <camilasouza@apples-MacBook-Pro.local>
2020-07-27 18:14:02 -0300

    finalização do exercício semana3-aula9

--- semana3/variaveis/js-vanilla-template/src/index.js
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -5,3 +5,66 @@ Exercícios de interpretação:
 2.  console.log(a, b, c) imprime 10 20 10
 */
 
+/*
+Exercícios de escrita de código
+*/
+
+/*01.*/
+
+/*
+const nome =
+let idade =
+
+console.log(typeof(nome), typeof(idade))
+*/
+/* 
+Sem atribuir um valor, a impressão no console dá erro
+*/
+/*
+const nome = prompt("Qual é o seu nome?")
+let idade = prompt("Qual a sua idade?")
+console.log(nome, idade)
+*/
+
+/*02*/
+
+/*
+const nome = prompt("Qual é o seu nome?")
+let idade = prompt("Qual é a sua idade?")
+let cor = prompt("Qual é a sua cor favorita?")
+const estadoNascimento = prompt("Em qual Estado você nasceu?")
+let estadoAtual = prompt("Em qual Estado você mora atualmente?")
+console.log(nome)
+console.log(idade)
+console.log(cor)
+console.log(estadoNascimento)
+console.log(estadoAtual)
+console.log(nome, idade, cor, estadoNascimento, estadoAtual)
+*/
+
+/*03*/
+
+/*
+let comidas = ["sushi", "sorvete", "caqui", "pizza", "tomate"]
+console.log(comidas)
+console.log("Essas são as minhas comidas preferidas:")
+console.log(comidas[0])
+console.log(comidas[1])
+console.log(comidas[2])
+console.log(comidas[3])
+console.log(comidas[4])
+let comidaUsuario = prompt("Qual a sua comida favorita?")
+console.log(comidas[0])
+console.log(comidaUsuario)
+console.log(comidas[2])
+console.log(comidas[3])
+console.log(comidas[4])
+*/
+
+/*4*/
+
+let covid = ["Você está com falta de ar?", "Você está com febre?", "Você testou positivo para Covid-19?"]
+let respostas = [true, false]
+console.log (covid[0], respostas[0])
+console.log (covid[1], respostas[1])
+console.log (covid[2], respostas[0])
