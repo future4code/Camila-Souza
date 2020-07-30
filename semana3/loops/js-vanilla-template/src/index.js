b21d1afbef3f3699dba8bcd639a8f1b3489b8d72
Mila <camilasouza@apples-MacBook-Pro.local>
2020-07-30 16:19:42 -0300

    laços

---
+++ semana3/loops/js-vanilla-template/src/index.js
@@ -0,0 +1,72 @@
+//Exercícios de interpretação de código
+/*
+Exercício 1
+O código está criando uma lista ordenada de números sequenciais usando laços. 
+0, 1, 2, 3, 4
+
+Exercício 2
+a.  
+    19    
+    21  
+    23    
+    25  
+    27    
+    30
+b.
+    Sim
+    const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+
+    for (let valor of lista) {
+		console.log(valor)
+	}
+}
+
+Desafio 1
+
+*/
+
+//Exercícios de escrita de código
+
+//Exercício 3
+
+const array = [13, 16, 12, 76, 84, 23, 10]
+//a.
+    // for (let i of array) {
+    //     console.log(i)
+    // }
+
+//b.
+    // for (let i of array) {
+    //     console.log(i / 10)
+    // }
+
+//c.
+//     for (let elementoDoArray of array) {
+//         if (elementoDoArray % 2 === 0) {
+//         console.log(elementoDoArray)
+//     }
+//  }    
+
+//d.
+    // const novoArray = []
+    // let index = 0
+    // for (let i of array) {
+    // novoArray.push("O elemento é: " + i + "e o index é: " + index)
+    // index++
+    // }
+    // console.log(novoArray)
+
+//e.
+//     let valorMaximo = 0
+//     for (let iMax of array) {
+//         if (iMax > valorMaximo) {
+//             valorMaximo = iMax
+//   }
+// }
+//     let valorMinimo = valorMaximo
+//     for (let iMin of array) {
+//         if (iMin < valorMinimo) {
+//             valorMinimo = iMin
+//   }
+// }
+//     console.log ("O maior número é " + valorMaximo + " e o menor é " + valorMinimo)
