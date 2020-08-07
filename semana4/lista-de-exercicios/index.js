23192dab9571fec7fde29a854b512127c390eeb3
Mila <camilasouza@apples-MacBook-Pro.local>
2020-08-07 13:39:42 -0300

    Exercício de interpretação de código - 1 mais detalhado

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,6 +1,9 @@
 /*
 Exercícios de interpretação de código
-    1. R$543
-        Obs.: Peguei a cotação do dólar no google e vou supor que a pessoa inseriu esse valor no prompt (5,43)
+    1.  a. Puxa o prompt com a pergunta para informar o valor da cotação do dólar
+        b. Guarda o número em uma constante chamada cotacao.
+            Obs.: Peguei a cotação do dólar no google e vou supor que a pessoa inseriu esse valor no prompt (5,43)
+        c. Guarda a multiplicação do valor R$ valor em dolar, no caso, 100, com a cotação, 5,43
+        d. Imprime no console:  R$543
     2. 
 */
