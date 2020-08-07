8e740c43189ea09d5ccc06f7dfbef5bb60cbf5d0
Mila <camilasouza@apples-MacBook-Pro.local>
2020-08-07 13:46:55 -0300

    interpretação de código - 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -5,5 +5,11 @@ Exercícios de interpretação de código
             Obs.: Peguei a cotação do dólar no google e vou supor que a pessoa inseriu esse valor no prompt (5,43)
         c. Guarda a multiplicação do valor R$ valor em dolar, no caso, 100, com a cotação, 5,43
         d. Imprime no console:  R$543
-    2. 
+    2.  Para a const novoMontante:
+            a. Puxa o case Ações do switch(tipoDeInvestimento), onde multiplica o valor informado, 150, por 1.1
+            b. Imprime no console 165
+        Para a const segundoMontante:
+            a. Puxa o case default do switch(tipoDeInvestimento)
+            b. Imprime no console: "TIPO DE INVESTIMENTO INFORMADO INCORRETO!"
+    3.
 */
